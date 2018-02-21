c35b42b8746e4eecd201ad2564ee8582696c1353
rickellis <rickellis@gmail.com>
2018-02-21 14:27:21 -0700

    Cleaned up a few things

--- sheading.sh
+++ sheading.sh
@@ -7,12 +7,11 @@
 #      Shell Heading Generator   |___/ 
 #
 #-----------------------------------------------------------------------------------
-VERSION="1.2.5"
+VERSION="1.2.6"
 #-----------------------------------------------------------------------------------
 #
-# Generates a heading with the specified background color. 
-# The heading will span the entire width of the terminal regardless of size.
-# Can also randomly selectcolor background
+# Generates a heading with the specified background color. The heading will span the
+# entire width of the terminal regardless of size.Can also randomly select BG color.
 #
 #-----------------------------------------------------------------------------------
 # Author:   Rick Ellis
@@ -22,7 +21,7 @@ VERSION="1.2.5"
 #
 # USAGE:
 #
-#   sheading <color> "Heading Text"  # Specifig color
+#   sheading <color> "Heading Text"  # Specific color
 #   sheading random  "Heading Text"  # Random color
 #
 # COLORS:
@@ -45,80 +44,102 @@ VERSION="1.2.5"
 # sheading org "Orange"
 # sheading pnk "Pink"
 # sheading lav "Lavender"
-# sheading mag "Magenta"
+# sheading mag "Magenta"3
 # sheading pur "Purple"
 #
 #-----------------------------------------------------------------------------------
 
-function sheading() {
+sheading() {
+
     if [ -z "$1" ] || [ -z "$2" ]; then
         echo 'Usage: heading <color> "Heading Text"'
         exit 1
     fi
 
-    _color=${1,,} # lowercase
-    _heading=${2}
-    _h_length=${#_heading} # heading length
-    _t_width=$(tput cols)  # terminal width
+    color=${1,,}        # lowercase
+    hding=${2}          # Capture heading
+    hdlen=${#hding}     # heading length
+    twidt=$(tput cols)  # terminal width
 
-    # Set the minimum width the length of the header string
-    if [ ! $_t_width -gt $_h_length ]; then
-        _t_width=$_h_length
+    # Set the minimum width to match length of the heading
+    if [ ! $twidt -gt $hdlen ]; then
+        twidt=$hdlen
     fi
 
     # Calculate the padding necessary on either side of the heading
-    l=$(( _t_width - _h_length )) 
+    l=$(( twidt - hdlen )) 
     d=$(( l / 2 ))
 
-    _padding=""
+    padding=""
     for i in $(seq 1 ${d}); do 
-        _padding+=" "
+        padding+=" "
     done
 
-    # Depending on the length of the terminal relative to the
-    # length of the heading we might end up one character off.
-    # To compensate we add one space to the right side
-    _padextra=""
-    _padlenth=${#_padding}
-    _totlenth=$(( _padlenth * 2 + _h_length ))
-
-    if [ $_t_width -ne $_totlenth ]; then
-        _padextra=" ";
+    # Thanks to Bash's auto-rounding, depending on the length of the
+    # terminal relative to the length of the heading we might end up
+    # one character off. To compensate we add one space if necessary
+    padextra=""
+    padlenth=${#padding}
+    totlenth=$(( padlenth * 2 + hdlen ))
+    if [ $twidt -ne $totlenth ]; then
+        padextra=" ";
     fi
 
-    if [ "$_color" == 'rnd' ] || [ "$_color" == "rand" ] || [ "$_color" == "random" ]; then
-        _colors=( "gry" "chr" "red" "grn" "lim" "aqm" "olv" "blu" "sky" "cyn" "aqa" "gdr" "yel" "crl" "org" "pnk" "lav" "mag" "pur" )
-        _color=${_colors[$RANDOM % ${#_colors[@]}]}
+    # Random color generator
+    if [ "$color" == 'rnd' ] || [ "$color" == "rand" ] || [ "$color" == "random" ]; then
+        colors=(   
+                    "gry" 
+                    "chr"
+                    "red"
+                    "grn"
+                    "lim"
+                    "aqm"
+                    "olv"
+                    "blu"
+                    "sky"
+                    "cyn"
+                    "aqa"
+                    "gdr"
+                    "yel"
+                    "crl"
+                    "org"
+                    "pnk"
+                    "lav"
+                    "mag" 
+                    "pur"
+                )
+
+        color=${colors[$RANDOM % ${#colors[@]}]}
     fi
 
-    # White text \e[97m
+    # White text: \e[97m
     # Black text: \e[38;5;232m
 
-    case "$_color" in
-        grey | gry)         _color="\e[48;5;240m\e[97m"            ;;
-        charcoal | chr)     _color="\e[48;5;237m\e[97m"            ;;
-        red)                _color="\e[48;5;1m\e[97m"              ;;
-        green | grn)        _color="\e[48;5;22m\e[97m"             ;;
-        lime | lim)         _color="\e[48;5;40m\e[38;5;232m"       ;;
-        aquamarine | aqm)   _color="\e[48;5;120m\e[38;5;232m"      ;;
-        olive | olv)        _color="\e[48;5;58m\e[97m"             ;;
-        blue | blu)         _color="\e[44m\e[97m"                  ;;
-        sky)                _color="\e[48;5;25m\e[97m"             ;;
-        cyan | cyn)         _color="\e[46m\e[97m"                  ;;
-        aqua | aqa)         _color="\e[48;5;87m\e[38;5;232m"       ;;
-        goldenrod | gdr)    _color="\e[48;5;220m\e[38;5;232m"      ;;
-        yellow | yel)       _color="\e[48;5;11m\e[38;5;232m"       ;;
-        coral| crl)         _color="\e[48;5;3m\e[97m"              ;;
-        orange | org)       _color="\e[48;5;202m\e[97m"            ;;
-        pink | pnk)         _color="\e[48;5;200m\e[97m"            ;;
-        lavender | lav)     _color="\e[48;5;141m\e[38;5;232m"      ;;
-        magenta | mag)      _color="\e[45m\e[97m"                  ;;
-        purple | pur)       _color="\e[48;5;53m\e[97m"             ;;
-        *)                  _color="\e[48;5;237m\e[97m"            ;;
+    case "$color" in
+        grey | gry)         color="\e[48;5;240m\e[97m"            ;;
+        charcoal | chr)     color="\e[48;5;237m\e[97m"            ;;
+        red)                color="\e[48;5;1m\e[97m"              ;;
+        green | grn)        color="\e[48;5;22m\e[97m"             ;;
+        lime | lim)         color="\e[48;5;40m\e[38;5;232m"       ;;
+        aquamarine | aqm)   color="\e[48;5;120m\e[38;5;232m"      ;;
+        olive | olv)        color="\e[48;5;58m\e[97m"             ;;
+        blue | blu)         color="\e[44m\e[97m"                  ;;
+        sky)                color="\e[48;5;25m\e[97m"             ;;
+        cyan | cyn)         color="\e[46m\e[97m"                  ;;
+        aqua | aqa)         color="\e[48;5;87m\e[38;5;232m"       ;;
+        goldenrod | gdr)    color="\e[48;5;220m\e[38;5;232m"      ;;
+        yellow | yel)       color="\e[48;5;11m\e[38;5;232m"       ;;
+        coral| crl)         color="\e[48;5;3m\e[97m"              ;;
+        orange | org)       color="\e[48;5;202m\e[97m"            ;;
+        pink | pnk)         color="\e[48;5;200m\e[97m"            ;;
+        lavender | lav)     color="\e[48;5;141m\e[38;5;232m"      ;;
+        magenta | mag)      color="\e[45m\e[97m"                  ;;
+        purple | pur)       color="\e[48;5;53m\e[97m"             ;;
+        *)                  color="\e[48;5;237m\e[97m"            ;;
     esac
 
     echo
-    echo -e "${_color}${_padding}${_heading}${_padding}${_padextra}\e[0m"
+    echo -e "${color}${padding}${hding}${padding}${padextra}\e[0m"
     echo
 }
 
